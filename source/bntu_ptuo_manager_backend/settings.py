79d4231da2f4093ee2ba5096281724f6e4506aec
mivanoffka <sharariki@gmail.com>
2025-04-03 07:45:50 +0300

    feat: add swagger

--- source/bntu_ptuo_manager_backend/settings.py
+++ source/bntu_ptuo_manager_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
+    "drf_yasg",
     "users",
     "employees",
 ]
--- source/bntu_ptuo_manager_backend/urls.py
+++ source/bntu_ptuo_manager_backend/urls.py
@@ -15,11 +15,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from rest_framework import permissions
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version="v1",
+        description="Описание вашего API",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("employees/", include("employees.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
