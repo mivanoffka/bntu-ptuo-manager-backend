8c36bc7749a5e82f9f0cb6c77b0c69a34fb80f12
mivanoffka <sharariki@gmail.com>
2025-05-19 21:22:30 +0300

    feat: automatically create admin

--- src/users/apps.py
+++ src/users/apps.py
@@ -1,6 +1,32 @@
+from operator import is_
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
+from django.db import connections
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
+
+    def ready(self):
+
+        try:
+            db_conn = connections["default"]
+            db_conn.ensure_connection()
+        except OperationalError:
+            return
+
+        UserModel = get_user_model()
+        if not UserModel.objects.filter(is_superuser=True).exists():
+            print("Creating default superuser...")
+            try:
+                UserModel.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="admin",
+                    is_verified=True,
+                )
+                print("Superuser created.")
+            except Exception as e:
+                print("Error creating superuser:", e)
